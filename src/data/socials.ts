d5f063ee52caaa70ad53a9a21e0016ce57205e01
Joshua Mason <joshuam1008@gmail.com>
2026-03-14 22:15:18 -0500

    fix email

--- src/data/socials.ts
+++ src/data/socials.ts
@@ -6,8 +6,28 @@ export type Social = {
 };
 
 export const SOCIALS: Social[] = [
-  { id: 1, name: 'GitHub',   url: 'https://github.com/joshuam1008',                 label: 'github.com/joshuam1008' },
-  { id: 2, name: 'LinkedIn', url: 'https://www.linkedin.com/in/joshuam1008/',       label: 'linkedin.com/in/joshuam1008' },
-  { id: 3, name: 'Email',    url: 'mailto:joshuamason1008@gmail.com',               label: 'joshuamason1008@gmail.com' },
-  { id: 4, name: 'Resume',   url: '/Joshua-Mason-Resume.pdf',                       label: 'Joshua-Mason-Resume.pdf' },
+  {
+    id: 1,
+    name: "GitHub",
+    url: "https://github.com/joshuam1008",
+    label: "github.com/joshuam1008",
+  },
+  {
+    id: 2,
+    name: "LinkedIn",
+    url: "https://www.linkedin.com/in/joshuam1008/",
+    label: "linkedin.com/in/joshuam1008",
+  },
+  {
+    id: 3,
+    name: "Email",
+    url: "mailto:joshuam1008@gmail.com",
+    label: "joshuam1008@gmail.com",
+  },
+  {
+    id: 4,
+    name: "Resume",
+    url: "/Joshua-Mason-Resume.pdf",
+    label: "Joshua-Mason-Resume.pdf",
+  },
 ];
